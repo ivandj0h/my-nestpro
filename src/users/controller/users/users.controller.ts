fc05b463d35fc883647829b56cf76f71e50634e2
ivandjoh <ivandi.djoh@gmail.com>
2023-03-18 01:22:32 +0800

    Controller & README updated!

--- src/users/controller/users/users.controller.ts
+++ src/users/controller/users/users.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
+  // Get All Users
   @Get()
   getAllUsers() {
     return [
@@ -14,6 +15,31 @@ export class UsersController {
         id: 2,
         username: 'user',
         email: 'user@user.com',
+      },
+    ];
+  }
+
+  // Get All User's Posts
+  @Get('posts')
+  getAllUsersPosts() {
+    return [
+      {
+        id: 1,
+        userId: 1,
+        title: 'Post 1',
+        body: 'Post 1 Body',
+      },
+      {
+        id: 2,
+        userId: 1,
+        title: 'Post 2',
+        body: 'Post 2 Body',
+      },
+      {
+        id: 3,
+        userId: 2,
+        title: 'Post 3',
+        body: 'Post 3 Body',
       },
     ];
   }
