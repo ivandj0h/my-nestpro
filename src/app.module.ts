d8dd1fb33e610bf20748ddbf76fba7ddc088501b
ivandjoh <ivandi.djoh@gmail.com>
2023-03-18 00:54:08 +0800

    env added!

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
