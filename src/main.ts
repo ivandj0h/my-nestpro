b9a89ad7dfbe458e2c242218982e715102ca93c4
ivandjoh <ivandi.djoh@gmail.com>
2023-03-19 16:00:44 +0800

    fixed: can't resolve ConfigService

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(9999);
+  const logger = new Logger('Bootstrapper');
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const config = app.get<ConfigService>(ConfigService);
+
+  const port = config.get('APP_PORT');
+  await app.listen(Number(port));
+
+  logger.log(`App listening on port ${port}`);
 }
 bootstrap();
