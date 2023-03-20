3c1ec67f45851f08905f6374e680358746e688d9
ivandjoh <ivandi.djoh@gmail.com>
2023-03-21 00:21:06 +0800

    add token

--- src/users/middlewares/exampe/exampe.middleware.ts
+++ src/users/middlewares/exampe/exampe.middleware.ts
@@ -1,18 +1,28 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NestMiddleware,
+} from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
-import * as process from "process";
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { AppModule } from '../../../app.module';
 
 @Injectable()
 export class ExampeMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
+  async use(req: Request, res: Response, next: NextFunction) {
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+    const config = app.get<ConfigService>(ConfigService);
+
+    const token = config.get('APP_TOKEN');
     const { authorization } = req.headers;
-    console.log(authorization);
-    if (!authorization) {
+
+    if (!authorization)
       return res.status(401).json({ message: 'No token provided' });
-    } else if (
-      authorization === process.env.TOKEN
-      // 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxODUyLCJlbWFpbCI6ImZyYW5zaXNrdXMuaGVucnlAc2VsbGVyaS5pZCIsIm5hbWUiOiJGcmFucyJ9LCJzdXBwbGllciI6eyJpZCI6MTc0NSwibmFtZSI6IlFBIFRlc3RpbmcifSwiaXNzIjoiU2VsbGVyaSIsImlhdCI6MTY2NDc4MjEyOSwiZXhwIjoxNjY1Mzg2OTI5LCJhdWQiOiJodHRwczovL3NlbGxlcmkuaWQifQ.eXvoPpCxCXNSYWmSOWuj-fiS59eQS9VtYzZZtaNIZvGux1AdlI9nm_x69a_PNAuHTT7S4lALt0jq2D__UM6YiA'
-    )
-      next();
+
+    if (authorization === token) next();
+    else throw new HttpException('Invalid token', HttpStatus.UNAUTHORIZED);
   }
 }
