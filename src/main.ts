05dc61044b2ce7c7fbf5dce69117e701d68f5513
ivandjoh <ivandi.djoh@gmail.com>
2023-03-22 15:03:07 +0800

    Swagger added!

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,28 @@ import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const logger = new Logger('Bootstrapper');
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const config = app.get<ConfigService>(ConfigService);
-
   const port = config.get('APP_PORT');
+  const swagger = new DocumentBuilder()
+    .setTitle('My NestPRO')
+    .setDescription('Learn the core fundamental concepts of NestJS')
+    .setVersion('1.0.0')
+    .addTag('NestJS')
+    .build();
+  const document = SwaggerModule.createDocument(app, swagger);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(Number(port));
 
   logger.log(`App listening on port ${port}`);
 }
-bootstrap();
+
+const promise = bootstrap();
+promise.catch((err) => {
+  console.error(err);
+});
